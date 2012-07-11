c2525ced378b90658e1d4172100a209e8a9550f4
lorencr7 <eden__lorenzo@hotmail.com>
2012-07-11 12:25:30 +0200

    first commit

---
+++ DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  TestApp
+//
+//  Created by Lorenzo Villarroel on 11/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  TestApp
+//
+//  Created by Lorenzo Villarroel on 11/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
