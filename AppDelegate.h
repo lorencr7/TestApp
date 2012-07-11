fbc3cd6106eb1a3695ea0f2af269e158475f3d33
lorencr7 <eden__lorenzo@hotmail.com>
2012-07-11 12:28:49 +0200

    second commit

--- AppDelegate.h
+++ AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Lorenzo Villarroel on 11/07/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-
+//hola
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
